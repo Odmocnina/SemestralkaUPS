76626595ee367acf018522a9fe1c342d1e991067
Odmocnina <145841370+Odmocnina@users.noreply.github.com>
2024-10-06 23:19:54 +0200

    Upravení metody na přípravu grafiky

--- src/main/java/upsSP/GUI/OknoHra.java
+++ src/main/java/upsSP/GUI/OknoHra.java
@@ -19,29 +19,22 @@ public class OknoHra extends JPanel {
     public OknoHra() {
     }
 
-    private void prepareGraphics(Graphics2D g2d) {
-        // Anti-aliasing
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Get the size of the panel
-        float width = getWidth();
-        float height = getHeight();
-
-        // Translate the origin to the center of the panel
-        g2d.translate(width / 2, height / 2);
-
-        // Scale based on the size of the window
-        double scaleX = width / Konstanty.SIRKA_OKNA;
-        double scaleY = height / Konstanty.VYSKA_OKNA;
-        double scale = Math.min(scaleX, scaleY);
-        g2d.scale(scale, scale);
+    private void pripravGrafiku(Graphics2D g) {
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        float sirka = getWidth();
+        float vyska = getHeight();
+        g.translate(sirka / 2, vyska / 2);
+        double meritkoX = sirka / Konstanty.SIRKA_OKNA;
+        double meritkoY = vyska / Konstanty.VYSKA_OKNA;
+        double mensi = Math.min(meritkoX, meritkoY);
+        g.scale(mensi, mensi);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        prepareGraphics(g2);
+        pripravGrafiku(g2);
         setBackground(new Color(0, 128, 0));
         //setLayout(new GridLayout(poctetRadku, pocetSloupcu));
         IVolba[] volby = new IVolba[Konstanty.POCET_MOZNOSTI + 1];
